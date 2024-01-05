fea1fbd68b011091abd8270c5ee10ab65b06f6ff
Lahcen Hicham <83353518+lahsen555@users.noreply.github.com>
2024-01-05 00:18:13 -1200

    Update Home.py

--- Home.py
+++ Home.py
@@ -16,6 +16,14 @@ st.set_page_config(
     initial_sidebar_state="expanded"
     )
 
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
 #alt.themes.enable("dark")
 
 
